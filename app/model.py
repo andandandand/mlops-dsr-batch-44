cd952556178f1729924605a543f32765ff70a110
Antonio Rueda-Toicen <antonio.rueda.toicen@gmail.com>
2025-12-12 14:44:58 +0000

    fc

--- app/model.py
+++ app/model.py
@@ -10,6 +10,7 @@ MODEL_FILENAME = "best_model.pth"
 os.makedirs(MODELS_DIR, exist_ok=True)
 
 
+<<<<<<< HEAD
 def download_model():
     """
     Download a model artifact from Weights & Biases.
@@ -27,10 +28,16 @@ def download_model():
     Returns:
         wandb.Artifact: The downloaded artifact object
     """
+=======
+def download_artifact():
+    # this will raise an assertion error if the variable is not found
+    assert 'WANDB_API_KEY' in os.environ, "WANDB_API_KEY not found in environment variables"
+>>>>>>> 01d1a7a (adding changes to hello.py and model.py)
     wandb_org = os.environ.get("WANDB_ORG")
     wandb_project = os.environ.get("WANDB_PROJECT")
     wandb_model_name = os.environ.get("WANDB_MODEL_NAME")
     wandb_model_version = os.environ.get("WANDB_MODEL_VERSION")
+<<<<<<< HEAD
     
     wandb_api_key = os.getenv("WANDB_API_KEY")
     wandb.login(key=wandb_api_key)
@@ -45,3 +52,22 @@ def download_model():
 
 if __name__ == "__main__":
     download_model()
+=======
+
+
+    wandb_api_key = os.getenv("WANDB_API_KEY")
+    wandb.login(key=wandb_api_key)
+    api = wandb.Api()
+
+    #artifact_path = "username/project_name/artifact_name:version"
+    # TODO: ensemble the artifact path from environment variables
+    #artifact_path = "antonios-org/mlops_dsr_batch_44/resnet18:v0"
+    artifact_path = f"{wandb_org}/{wandb_project}/{wandb_model_name}:{wandb_model_version}"
+    artifact = api.artifact(artifact_path, type="model")
+    print(f"Downloading artifact from {artifact_path}")
+
+    artifact.download(root=MODELS_DIR)
+
+
+download_artifact()
+>>>>>>> 01d1a7a (adding changes to hello.py and model.py)
